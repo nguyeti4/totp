62b1603c97a1649aa0e2ad45f77f746d0a5baba5
Timothy Nguyen <timothynguyen2000@yahoo.com>
2021-05-23 00:46:09 -0700

    created example key

--- otp.py
+++ otp.py
@@ -8,7 +8,8 @@ from pyqrcode import QRCode
 if str(sys.argv[1]) == 'generate-otp':
 
     #creates random secret key
-    key = pyotp.random_base32()
+    #key = pyotp.random_base32()
+    key = 'JBSWY3DPEHPK3PXP'
 
     #assume user id as 'alice@google.com' and issuer is 'Secure App'. Generate uri
     uri = pyotp.totp.TOTP(key).provisioning_uri(name='alice@google.com', issuer_name='Secure App')
@@ -23,7 +24,8 @@ if str(sys.argv[1]) == 'generate-otp':
 if str(sys.argv[1]) == 'generate-otp':
 
     #Generate otp
-    totp = = pyotp.TOTP(key)
+    #totp = = pyotp.TOTP(key)
+    totp = = pyotp.TOTP('JBSWY3DPEHPK3PXP')
 
     #print to screen the current totp
     print("Current OTP:",totp.now())
