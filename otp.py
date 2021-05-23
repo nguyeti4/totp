b722dd6cbf26a7db35c3e4c4d485a5797ea99f3f
Timothy Nguyen <timothynguyen2000@yahoo.com>
2021-05-23 00:35:58 -0700

    created otp.py

---
+++ otp.py
@@ -0,0 +1,29 @@
+import pyqrcode
+import png
+import pyotp
+import sys
+from pyqrcode import QRCode
+
+#if python otp.py generate-otp
+if str(sys.argv[1]) == 'generate-otp':
+
+    #creates random secret key
+    key = pyotp.random_base32()
+
+    #assume user id as 'alice@google.com' and issuer is 'Secure App'. Generate uri
+    uri = pyotp.totp.TOTP(key).provisioning_uri(name='alice@google.com', issuer_name='Secure App')
+
+    #generate qr code based off uri
+    url = pyqrcode.create(uri)
+
+    # Create and save the png file naming "myqr.png"
+    url.png('myqr.png',scale=6)
+
+#if python otp.py get-otp
+if str(sys.argv[1]) == 'generate-otp':
+
+    #Generate otp
+    totp = = pyotp.TOTP(key)
+
+    #print to screen the current totp
+    print("Current OTP:",totp.now())
